f30b7cf62e9f69c871bfc6e391238757fed11e85
كارل مبارك <bonjour@moubarak.eu>
2020-01-16 17:26:15 +0100

    wikitext parsing works, cleaned up code

--- tool_body.php
+++ tool_body.php
@@ -8,11 +8,11 @@ class tool {
 		// $repo = $args['repo']
 		// $filname = $args['filename']
 		// $branch = $args['branch']
+		
+		$parser->getOutput()->addModules( 'ext.tool' );
 
 		$text = '{{#github:readme.md|hackersanddesigners/hdsa2019doc}}';
 		$jeroen = $parser->recursiveTagParse( $text, $frame );
-
-		$parser->getOutput()->addModules( 'ext.tool' );
 
 		$ret = '<table class="toolTable">';
 		$ret .= '<tr class="toolHeader">';
@@ -20,7 +20,6 @@ class tool {
 		$ret .= '</tr>';
     $ret .= '<tr class="toolContent">';
     $ret .= '<td>' . $jeroen . '</td>';
-		$ret .= '<td>Figure out how to integrate Jeroens github api here wih args</td>';
     $ret .= '</tr>';
 		$ret .= '<tr class="toolFooter">';
 		$ret .= '<td><a href="">view more tools</a></td>';
