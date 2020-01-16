6165b701b1e900fd74fd1fdae33ad3a3dcec7f4f
كارل مبارك <bonjour@moubarak.eu>
2020-01-16 23:12:09 +0100

    css

--- tool_body.php
+++ tool_body.php
@@ -19,7 +19,7 @@ class tool {
 
 		$ret = '<table class="toolTable">';
 		$ret .= '<tbody>';
-		$ret .= "<th class='toolHeader'><a href='{$github}/{$user}' target='_blank'>{$user}</a> / <a href='{$github}/{$user}/{$repo}' target='_blank'>{$repo}</a> / {$file}</th>";
+		$ret .= "<th class='toolHeader' colspan='2'><a href='{$github}/{$user}' target='_blank'>{$user}</a> / <a href='{$github}/{$user}/{$repo}' target='_blank'>{$repo}</a> / {$file}</th>";
 		$ret .= "<tr class='toolContent'><td colspan='2'>{$jeroen}</td></tr>";
 		$ret .= '<tr class="toolFooter">';
 		$ret .= '<td><a href="https://hackersanddesigners.nl/s/Tools">view more tools</a></td>';
