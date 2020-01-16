fc0c48c591a695d567d39c3e258d49e7130952f1
كارل مبارك <bonjour@moubarak.eu>
2020-01-16 16:56:29 +0100

    created attributes for tool tag

--- tool_body.php
+++ tool_body.php
@@ -5,16 +5,19 @@ class tool {
 		return true;
 	}
 	static function toolRender( $input, array $args, Parser $parser, PPFrame $frame ) {
+		$repo = $args['repo']
+		$filname = $args['filename']
+		$branch = $args['branch']
+
 		$parser->getOutput()->addModules( 'ext.tool' );
+
 		$ret = '<table class="toolTable">';
 		$ret .= '<tr class="toolHeader">';
 		$ret .= '<td>GithubRepository/Path/Filename</td>';
 		$ret .= '</tr>';
-		$ret .= '<tr>';
-		$ret .= '<td>FileContents (Jeroens Syntax)</td>';
-		$ret .= '</tr>';
     $ret .= '<tr class="toolContent">';
     $ret .= '<td>{{#github:readme.md|hackersanddesigners/hdsa2019doc}}</td>';
+		$ret .= '<td>Figure out how to integrate Jeroens github api here wih args</td>';
     $ret .= '</tr>';
 		$ret .= '<tr class="toolFooter">';
 		$ret .= '<td><a href="">view more tools</a></td>';
