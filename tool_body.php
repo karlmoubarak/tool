eff23d78faba4ea91e54e6b839bd570e93c0f98c
كارل مبارك <bonjour@moubarak.eu>
2020-01-20 11:35:38 +0100

    css

--- tool_body.php
+++ tool_body.php
@@ -19,9 +19,8 @@ class tool {
 		$ret .= "<div class='toolHeader'><a href='{$github}/{$user}' target='_blank'>{$user}</a> / <a href='{$github}/{$user}/{$repo}' target='_blank'>{$repo}</a> / {$file}</div>";
 		$ret .= "<div class='toolContent'>{$jeroen}</div>";
 		$ret .= '<div class="toolFooter">';
-		$ret .= '<p><a href="https://hackersanddesigners.nl/s/Tools">view more tools</a></p>';
-		$ret .= '<p>/</p>'
-		$ret .= '<p><a href="' . $url . '" target="_blank">↗ view in GitHub ↗</a></p>';
+		$ret .= '<a href="https://hackersanddesigners.nl/s/Tools">view more tools</a>';
+		$ret .= '<a class="inGitHub" href="' . $url . '" target="_blank">↗ view in GitHub ↗</a>';
 		$ret .= '</div>';
 		$ret .= '</div>';
     // $ret .= '<p>also appears in:</p>';
