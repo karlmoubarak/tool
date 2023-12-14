a4db9b68079a3aa58b489dd2a5ae150653e0378a
كارل مبارك <bonjour@moubarak.eu>
2023-12-14 21:52:11 +0100

    update to use new embed content extension

--- tool_body.php
+++ tool_body.php
@@ -12,7 +12,7 @@ class tool {
 
 		$url = $github . '/' . $user . '/' . $repo;
 
-		$text = '{{#embed:' . $url . '}}';
+		$text = '{{#embed:' . $url . '/blob/master/' . $file . '}}';
 		// $text = '{{#embed:' . 'https://raw.githubusercontent.com/hackersanddesigners/hdsa2019doc/master/readme.md' . '}}';
 		$jeroen = $parser->recursiveTagParse( $text, $frame );
 
