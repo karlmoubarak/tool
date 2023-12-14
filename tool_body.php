73117f6532190bab8ccedd62a707dcdec6942548
كارل مبارك <bonjour@moubarak.eu>
2023-12-14 18:16:47 +0100

    update to use new embed content extension

--- tool_body.php
+++ tool_body.php
@@ -12,7 +12,7 @@ class tool {
 
 		$url = $github . '/' . $user . '/' . $repo;
 
-		$text = '{{#embed:' . $github . '/' . $user . '/' . $repo . '}}';
+		$text = '{{#github:' . $github . '/' . $user . '/' . $repo . '}}';
 		$jeroen = $parser->recursiveTagParse( $text, $frame );
 
 		$ret = '<tool>';
